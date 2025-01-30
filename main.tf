fe2215f4dc9a4d302f312c924b59cee51a7ac069
AJAY KUMAR <99399155+ajaydevtron@users.noreply.github.com>
2025-01-30 16:33:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,27 @@
 
+provider "google" {
+  project = "devtron-non-prod"  # Replace with your GCP project ID
+  region  = "asia-south1"
+  zone    = "asia-south1-a"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "gaia-vm"
+  machine_type = "e2-micro"  # Fixed instance type
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
+
+output "instance_ip" {
+  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+}
